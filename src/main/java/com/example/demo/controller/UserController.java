d24e2cc64088445e5e7e3eb7d1810f024775f427
DELL <syedabdullah199758@gmail.com>
2022-01-31 08:40:09 +0500

    second commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.AppUser;
 import com.example.demo.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 private  final IUserService userService;
+
+
+@GetMapping(path = "/user")
+public ResponseEntity<List<AppUser>> getUsers(){
+       return ResponseEntity.ok(userService.getUser());
+}
 
 }
